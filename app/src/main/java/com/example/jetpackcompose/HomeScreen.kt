e8331c2bd57a3b29473749eb1948fb50335b2c54
hanselgunawan <hanselgunawan94@gmail.com>
2021-08-28 16:25:44 -0700

    Added resources for Meditation App

---
+++ app/src/main/java/com/example/jetpackcompose/HomeScreen.kt
@@ -0,0 +1,62 @@
+package com.example.jetpackcompose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(Color.Blue)
+            .fillMaxSize()
+    ) {
+        Column() {
+            
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Philipp"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day!",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(
+            painter = painterResource(id = ), 
+            contentDescription = 
+        )
+    }
+}
